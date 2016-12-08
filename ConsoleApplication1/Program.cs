6a6e83ffac3bb0762970910d7847cc90fabee4d7
Patrick Todd <patrick.todd@reisystems.com>
2016-12-08 17:34:34 -0500

    string and array modifications

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //convert array of integers into string, then concat string
+            //int[] numArr = {0,1,2,3,4,5};
+            //var arr = string.Join(",", numArr);
+
+            //convert array on char to string, then concats string
+            //char[] charArr = { 'a', 'b', 'c' };
+            //var arr = new String(charArr);
+
+
+            Console.WriteLine("the array values: " + arr);
+            Console.ReadKey();
+        }
+    }
+}
